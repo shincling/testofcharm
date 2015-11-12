f1d8fc9fe4d4393f8a7aa3edb90748b1cfcbb994
shincling <shijing609@163.com>
2015-11-12 15:33:21 +0800

    data parser of the rand 1000 of plane

--- dataparse.py
+++ dataparse.py
@@ -1,13 +1,31 @@
 #-*- coding:utf-8 -*-
 import random
-f=open(r'/home/shin/DeepLearning/数据集/qa23_plane_train','r')
-content=f.read()
-print content
+import re
 
-fw=open(r'/home/shin/DeepLearning/数据集/qa23_plane_train_rand','w')
-for i in range(1000):
-    content_rand=content.replace('idnumber',str(random.randint(10000000,99999999)))
-    content_rand=content_rand.replace('timetime','20'+str(random.randint(10,15))+'-'+str(random.randint(1,12))+'-'+str(random.randint(1,31)))
-    fw.write(content_rand)
+namedict=['张三','李四','王五','赵六','田七','许八','钟九','周杰伦','解晓东','蔡依林']
+f=open(r'/home/shin/DeepLearning/数据集/qa24_plane_little_train.txt','r')
+fw=open(r'/home/shin/DeepLearning/数据集/qa24_plane_little_train_rand.txt','w')
+content=f.read()
+#print content
+ac=content.split('1 您好')
+ac=ac[1:-1]
+file=''
+for story in ac:
+    story='1 您好'+story
+    story=story.replace('373522197607234547',str(random.randint(10000000,99999999)))
+    story=story.replace('周涛',namedict[random.randint(0,9)])
+    file=file+story
+
+fw.write(file)
+f.close()
+fw.close()
+
+
+'''
+fw=open(r'/home/shin/DeepLearning/数据集/qa23_plane_train_rand.txt','w')
+content_rand=content.replace('373522197607234547',str(random.randint(10000000,99999999)))
+content_rand=content_rand.replace('timetime','20'+str(random.randint(10,15))+'-'+str(random.randint(1,12))+'-'+str(random.randint(1,31)))
+fw.write(content_rand)
 fw.close()
 f.close()
+'''
