7b2bdb73eab3b7eacf4e76f829325f87c7271bd4
shincling <shijing609@163.com>
2015-11-12 14:26:16 +0800

    class Rename

---
+++ calthediff.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+import re
+f_test=open(r'/home/shin/DeepLearning/数据集/qa22_plane_1000_test.txt','r')
+f_train=open(r'/home/shin/DeepLearning/数据集/qa22_plane_1000_train.txt','r')
+cont1=f_test.read()
+cont2=f_train.read()
+cont1=re.findall(r'[0-9]* 。',cont1)
+print cont1
