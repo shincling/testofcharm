f461259b14877560ee1d7e652a8ebdb21624f053
shincling <shijing609@163.com>
2015-11-12 10:34:24 +0800

    class Rename

--- hellowcharm.py
+++ hellowcharm.py
@@ -3,3 +3,9 @@ print 'hello charm'
 print 'what\'s the hell is going on now?'
 
 print 'ssss'
+
+class Rename(object):
+    def __init__(self,x,y0):
+        aa=y0
+    def fprint(self):
+        print
