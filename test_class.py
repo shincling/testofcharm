69d7a21eb58bfc6314cc5075e0f6267b5beae097
shincling <shijing609@163.com>
2016-02-14 18:16:49 +0800

    测试了几个调用。

--- test_class.py
+++ test_class.py
@@ -1,9 +1,17 @@
 class ttt():
-    def __init__(self,a,b,c):
+    def __init__(self,a,b=2,c=3):
         self.fi=a
         self.se=b
         self.th=c
+    def dd(self):
+        print self.th*2
 
-bb=ttt(1,2,3)
+bb=ttt(1)
 
 print bb.fi,bb.se,bb.th
+bb.dd()
+
+try:
+    print cc
+except Exception,e:
+    print e
